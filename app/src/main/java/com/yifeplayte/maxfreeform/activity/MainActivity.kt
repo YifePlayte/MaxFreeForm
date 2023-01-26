b7b351c972d6ed7cc86a1855731ae27795e171ce
YifePlayte <yife-playte@foxmail.com>
2023-01-26 19:50:57 +0800

    Refactor main page

--- app/src/main/java/com/yifeplayte/maxfreeform/activity/MainActivity.kt
+++ app/src/main/java/com/yifeplayte/maxfreeform/activity/MainActivity.kt
@@ -2,13 +2,10 @@ package com.yifeplayte.maxfreeform.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import cn.fkj233.ui.activity.MIUIActivity
-import cn.fkj233.ui.activity.view.SwitchV
-import cn.fkj233.ui.activity.view.TextSummaryV
 import cn.fkj233.ui.dialog.MIUIDialog
 import com.yifeplayte.maxfreeform.R
-import com.yifeplayte.maxfreeform.util.Utils
+import com.yifeplayte.maxfreeform.activity.pages.MainPage
 import kotlin.system.exitProcess
 
 class MainActivity : MIUIActivity() {
@@ -35,95 +32,7 @@ class MainActivity : MIUIActivity() {
     }
 
     init {
-        initView {
-            registerMain(getString(R.string.app_name), false) {
-                TitleText(textId = R.string.maxfreeform_tips)
-                TextSummaryWithSwitch(
-                    TextSummaryV(
-                        textId = R.string.side_hide_freeform,
-                        tipsId = R.string.side_hide_freeform_tips
-                    ),
-                    SwitchV("side_hide", true)
-                )
-                TextSummaryWithSwitch(
-                    TextSummaryV(
-                        textId = R.string.remove_conversation_bubble_settings_restriction,
-                        tipsId = R.string.remove_conversation_bubble_settings_restriction_tips
-                    ),
-                    SwitchV("side_hide_notification", true)
-                )
-                TextSummaryWithSwitch(
-                    TextSummaryV(
-                        textId = R.string.can_notification_slide,
-                        tipsId = R.string.can_notification_slide_tips
-                    ),
-                    SwitchV("can_notification_slide", true)
-                )
-                TextSummaryWithSwitch(
-                    TextSummaryV(
-                        textId = R.string.recents_to_small_freeform,
-                        tipsId = R.string.recents_to_small_freeform_tips
-                    ),
-                    SwitchV("recents_to_small_freeform", false)
-                )
-                Line()
-                TitleText(textId = R.string.more)
-                TextSummaryArrow(
-                    TextSummaryV(
-                        textId = R.string.try_to_fix_conversation_bubbles,
-                        tipsId = R.string.try_to_fix_conversation_bubbles_tips
-                    ) {
-                        Utils.exec("pm enable com.miui.securitycenter/com.miui.bubbles.services.BubblesNotificationListenerService")
-                        Toast.makeText(
-                            this@MainActivity,
-                            getString(R.string.finished),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                )
-                Line()
-                TitleText(textId = R.string.reboot)
-                TextA(
-                    textId = R.string.restart_all_scope,
-                    onClickListener = {
-                        MIUIDialog(this@MainActivity) {
-                            setTitle(R.string.warning)
-                            setMessage(R.string.restart_all_scope_tips)
-                            setLButton(R.string.cancel) {
-                                dismiss()
-                            }
-                            setRButton(R.string.done) {
-                                val command = arrayOf(
-                                    "killall com.android.systemui",
-                                    "killall com.miui.home",
-                                    "killall com.miui.securitycenter",
-                                )
-                                Utils.exec(command)
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    getString(R.string.finished),
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }.show()
-                    }
-                )
-                TextA(
-                    textId = R.string.reboot_system,
-                    onClickListener = {
-                        MIUIDialog(this@MainActivity) {
-                            setTitle(R.string.warning)
-                            setMessage(R.string.reboot_tips)
-                            setLButton(R.string.cancel) {
-                                dismiss()
-                            }
-                            setRButton(R.string.done) {
-                                Utils.exec("/system/bin/sync;/system/bin/svc power reboot || reboot")
-                            }
-                        }.show()
-                    }
-                )
-            }
-        }
+        activity = this
+        registerPage(MainPage::class.java)
     }
 }
---
+++ app/src/main/java/com/yifeplayte/maxfreeform/activity/pages/MainPage.kt
@@ -0,0 +1,104 @@
+package com.yifeplayte.maxfreeform.activity.pages
+
+import android.widget.Toast
+import cn.fkj233.ui.activity.annotation.BMMainPage
+import cn.fkj233.ui.activity.data.BasePage
+import cn.fkj233.ui.activity.view.SwitchV
+import cn.fkj233.ui.activity.view.TextSummaryV
+import cn.fkj233.ui.dialog.MIUIDialog
+import com.yifeplayte.maxfreeform.R
+import com.yifeplayte.maxfreeform.util.Utils
+
+@BMMainPage(titleId = R.string.app_name)
+class MainPage : BasePage() {
+    override fun onCreate() {
+        TitleText(textId = R.string.maxfreeform_tips)
+        TextSummaryWithSwitch(
+            TextSummaryV(
+                textId = R.string.side_hide_freeform,
+                tipsId = R.string.side_hide_freeform_tips
+            ),
+            SwitchV("side_hide", true)
+        )
+        TextSummaryWithSwitch(
+            TextSummaryV(
+                textId = R.string.remove_conversation_bubble_settings_restriction,
+                tipsId = R.string.remove_conversation_bubble_settings_restriction_tips
+            ),
+            SwitchV("side_hide_notification", true)
+        )
+        TextSummaryWithSwitch(
+            TextSummaryV(
+                textId = R.string.can_notification_slide,
+                tipsId = R.string.can_notification_slide_tips
+            ),
+            SwitchV("can_notification_slide", true)
+        )
+        TextSummaryWithSwitch(
+            TextSummaryV(
+                textId = R.string.recents_to_small_freeform,
+                tipsId = R.string.recents_to_small_freeform_tips
+            ),
+            SwitchV("recents_to_small_freeform", false)
+        )
+        Line()
+        TitleText(textId = R.string.more)
+        TextSummaryArrow(
+            TextSummaryV(
+                textId = R.string.try_to_fix_conversation_bubbles,
+                tipsId = R.string.try_to_fix_conversation_bubbles_tips
+            ) {
+                Utils.exec("pm enable com.miui.securitycenter/com.miui.bubbles.services.BubblesNotificationListenerService")
+                Toast.makeText(
+                    activity,
+                    getString(R.string.finished),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        )
+        Line()
+        TitleText(textId = R.string.reboot)
+        TextSummaryArrow(
+            TextSummaryV(
+                textId = R.string.restart_all_scope
+            ) {
+                MIUIDialog(activity) {
+                    setTitle(R.string.warning)
+                    setMessage(R.string.restart_all_scope_tips)
+                    setLButton(R.string.cancel) {
+                        dismiss()
+                    }
+                    setRButton(R.string.done) {
+                        val command = arrayOf(
+                            "killall com.android.systemui",
+                            "killall com.miui.home",
+                            "killall com.miui.securitycenter",
+                        )
+                        Utils.exec(command)
+                        Toast.makeText(
+                            activity,
+                            getString(R.string.finished),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }.show()
+            }
+        )
+        TextSummaryArrow(
+            TextSummaryV(
+                textId = R.string.reboot_system
+            ) {
+                MIUIDialog(activity) {
+                    setTitle(R.string.warning)
+                    setMessage(R.string.reboot_tips)
+                    setLButton(R.string.cancel) {
+                        dismiss()
+                    }
+                    setRButton(R.string.done) {
+                        Utils.exec("/system/bin/sync;/system/bin/svc power reboot || reboot")
+                    }
+                }.show()
+            }
+        )
+    }
+}
