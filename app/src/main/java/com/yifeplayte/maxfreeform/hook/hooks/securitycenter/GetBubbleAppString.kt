6278947ac41bf9c2aebc2432344a9b10680e5476
YifePlayte <yife-playte@foxmail.com>
2023-01-26 18:16:34 +0800

    Rename bubbleClass to classBubble

--- app/src/main/java/com/yifeplayte/maxfreeform/hook/hooks/securitycenter/GetBubbleAppString.kt
+++ app/src/main/java/com/yifeplayte/maxfreeform/hook/hooks/securitycenter/GetBubbleAppString.kt
@@ -8,7 +8,7 @@ import de.robv.android.xposed.XposedBridge
 object GetBubbleAppString : BaseHook() {
     override fun init() {
         try {
-            val bubbleClass = loadClass("com.miui.bubbles.Bubble")
+            val classBubble = loadClass("com.miui.bubbles.Bubble")
             findMethod("com.miui.bubbles.settings.BubblesSettings") {
                 name == "getBubbleAppString"
             }.hookBefore {
@@ -16,7 +16,7 @@ object GetBubbleAppString : BaseHook() {
                 val mActiveBubbles = it.thisObject.getObject("mActiveBubbles")
                 for (bubble in mActiveBubbles as HashSet<*>) {
                     stringBuilder.append(
-                        bubbleClass.getMethod("getPackageName").invokeAs<String>(bubble)
+                        classBubble.getMethod("getPackageName").invokeAs<String>(bubble)
                     )
                     stringBuilder.append(":")
                     stringBuilder.append(bubble.getObject("userId"))
