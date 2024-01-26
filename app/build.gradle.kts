f7fc02ca9a19df10a9d3af5359485d485d289ac7
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-01-26 19:20:06 +0800

    3.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.yifeplayte.maxfreeform"
         minSdk = 31
         targetSdk = 34
-        versionCode = 21
-        versionName = "3.2.3"
+        versionCode = 22
+        versionName = "3.2.4"
 
         applicationVariants.configureEach {
             outputs.configureEach {
